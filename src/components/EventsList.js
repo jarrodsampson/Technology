298d8dd454841f7ed51290f24412f2f055f38de2
Jarrod Sampson <jarrod@gunghoonline.com>
2017-04-12 18:58:10 -0700

    Init commit for Technology

---
+++ src/components/EventsList.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import OwlCarousel from 'react-owl-carousel';
+
+import Event from './Event';
+
+class EventsList extends Component {
+
+    render () {
+        var eventsList = this.props.data.map(function(event) {
+            //console.log(fanArt);
+            return (
+                <Event
+                    image={event.image}
+                    description={event.description}
+                    key={event.image}
+                />
+            );
+        }, this);
+        return (
+            <div className="photoList">
+                <OwlCarousel slideSpeed={300} items={4} itemsTablet={[1125,3]} itemsMobile={[700,2]} stopOnHover={true} lazyLoad={true} autoPlay={true} singleItem={false}>
+                    {eventsList}
+                </OwlCarousel>
+            </div>
+        );
+    }
+
+}
+
+export default EventsList;
